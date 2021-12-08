b58514de02872832a801bc589381be2f3db0e608
guthias <gmgustavom@gmail.com>
2021-12-08 17:03:45 -0300

    Criando a function arrayGenerator

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -40,6 +40,13 @@ const calculator = (number1, number2) => ({
   sub: Math.floor(number1 - number2),
 });
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  switch (type) {
+    case 'keys': return Object.keys(object);
+    case 'values': return Object.values(object);
+    case 'entries': return Object.entries(object);
+    default: return undefined;
+  }
+};
 
 module.exports = { calculator, arrayGenerator };
