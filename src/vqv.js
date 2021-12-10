752fb9d7af29a15c63490e96a89ca7cea33e564c
guthias <gmgustavom@gmail.com>
2021-12-10 15:11:04 -0300

    Simplificando comparacao na function  VQV

--- src/vqv.js
+++ src/vqv.js
@@ -17,9 +17,7 @@
 */
 
 const vqv = (name, age) => {
-  if (name === undefined || age === undefined) {
-    return undefined;
-  }
+  if (!name || !age) return undefined;
   return `Oi, meu nome é ${name}!
 Tenho ${age} anos,
 trabalho na Trybe e mando muito em programação!
